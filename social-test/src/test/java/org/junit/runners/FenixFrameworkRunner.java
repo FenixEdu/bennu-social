a778ad34e3835f8e3f6fd541cc63cafea0751ecc
Pedro Santos <pedro.san7os@gmail.com>
2016-08-30 10:58:56 +0100

    Included license headers

--- social-test/src/test/java/org/junit/runners/FenixFrameworkRunner.java
+++ social-test/src/test/java/org/junit/runners/FenixFrameworkRunner.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright © ${project.inceptionYear} Instituto Superior Técnico
+ *
+ * This file is part of social-test.
+ *
+ * social-test is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * social-test is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with social-test.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.junit.runners;
 
 import org.junit.runner.notification.RunNotifier;
